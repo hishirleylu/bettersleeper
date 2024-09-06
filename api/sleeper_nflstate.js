651b67ebfcb3a9819499565694267dddbb5a6938
Shirley Lu <70413049+hishirleylu@users.noreply.github.com>
2024-09-05 18:57:56 -0700

    Update to 2024

--- api/sleeper_nflstate.js
+++ api/sleeper_nflstate.js
@@ -1,8 +1,8 @@
 const NFL_STATE_ENDPOINT = "https://api.sleeper.app/v1/state/nfl"
 const NFL_PLAYERS_ENDPOINT = "https://api.sleeper.app/v1/players/nfl"
 const STATS_ENDPOINT = "https://api.sleeper.app/v1/stats/nfl/regular/2023/{0}" // 0: week
-const PROJECTIONS_ENDPOINT = "https://api.sleeper.app/v1/projections/nfl/regular/2023/{0}" // 0: week
-const NFL_SCHEDULE_ENDPOINT = "https://api.sleeper.app/schedule/nfl/regular/2023"
+const PROJECTIONS_ENDPOINT = "https://api.sleeper.app/v1/projections/nfl/regular/2024/{0}" // 0: week
+const NFL_SCHEDULE_ENDPOINT = "https://api.sleeper.app/schedule/nfl/regular/2024"
 
 async function getStats(week) {
     return apiCallOne(STATS_ENDPOINT, week);
